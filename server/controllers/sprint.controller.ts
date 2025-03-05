71e8e5214866daaab1f219d7388dd5a8d6f1f789
digiArchitect <quotethehero@gmail.com>
2025-03-05 14:22:48 -0500

    chore: sprint has the wrong name

--- server/controllers/sprint.controller.ts
+++ server/controllers/sprint.controller.ts
@@ -1,25 +1,24 @@
-import { FakeSOSocket } from "../types/types";
 import express, { Response, Request } from 'express';
+import { FakeSOSocket } from '../types/types';
 
-
-const projectController = (socket: FakeSOSocket) => {
+const sprintController = (socket: FakeSOSocket) => {
   const router = express.Router();
-  
-  const getProjectRoute = async (req: Request, res: Response): Promise<void> => {
+
+  const getSprintRoute = async (req: Request, res: Response): Promise<void> => {
     res.status(501).send('Not implemented');
   };
 
-  const createProjectRoute = async (req: Request, res: Response): Promise<void> => {
+  const createSprintRoute = async (req: Request, res: Response): Promise<void> => {
     res.status(501).send('Not implemented');
   };
 
-  const updateProjectRoute = async (req: Request, res: Response): Promise<void> => {
+  const updateSprintRoute = async (req: Request, res: Response): Promise<void> => {
     res.status(501).send('Not implemented');
   };
 
-  const deleteProjectRoute = async (req: Request, res: Response): Promise<void> => {
+  const deleteSprintRoute = async (req: Request, res: Response): Promise<void> => {
     res.status(501).send('Not implemented');
   };
 };
 
-export default projectController;
+export default sprintController;
