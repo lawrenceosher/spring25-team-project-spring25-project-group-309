41f0f3782d48d362261b601df8b24161def51234
AayushT824 <aayusht2003@gmail.com>
2025-03-26 12:23:07 -0400

    createProject

--- server/controllers/project.controller.ts
+++ server/controllers/project.controller.ts
@@ -1,16 +1,31 @@
 import express, { Response, Request } from 'express';
 import { FakeSOSocket } from '../types/types';
-import { getAllProjectsByUser } from '../services/project.service';
+import { getAllProjectsByUser, saveProject } from '../services/project.service';
 
 const projectController = (socket: FakeSOSocket) => {
   const router = express.Router();
+
+  const createProject = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const project = req.body;
+      const result = await saveProject(project);
+      if ('error' in result) {
+        throw new Error(`${result.error}`);
+      }
+
+      res.status(200).json(result);
+    }
+    catch (error) {
+      res.status(500).send(`Error when saving a project: ${(error as Error).message}`);
+    }
+  }
 
   const getProjectsByUser = async (req: Request, res: Response): Promise<void> => {
     try {
       const { username } = req.params;
 
       const projects = await getAllProjectsByUser(username);
-      const errorProject = projects.find(project => 'error' in projects);
+      const errorProject = projects.find(project => 'error' in project);
       if (errorProject) {
         throw new Error(`${errorProject}`);
       }
@@ -21,6 +36,7 @@ const projectController = (socket: FakeSOSocket) => {
     }
   };
   router.get('/:username', getProjectsByUser);
+  router.post('/createProject', createProject);
   return router;
 };
 
