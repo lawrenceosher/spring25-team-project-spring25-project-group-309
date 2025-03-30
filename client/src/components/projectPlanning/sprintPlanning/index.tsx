17b639ef94a86d90065d07c0655d08faf5e49873
Lawrence Osher <lao2205@gmail.com>
2025-03-30 18:52:33 -0400

    Refactored creating a new project

--- client/src/components/projectPlanning/sprintPlanning/index.tsx
+++ client/src/components/projectPlanning/sprintPlanning/index.tsx
@@ -2,7 +2,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import './index.css';
 import { useEffect, useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
 import {
   PopulatedDatabaseProject,
   PopulatedDatabaseSprint,
@@ -23,6 +22,7 @@ import SprintUpdateModal from '../components/SprintModals/SprintUpdateModal';
 import useUsersListPage from '../../../hooks/useUsersListPage';
 import { createProject, getProjectsByUser } from '../../../services/projectService';
 import useUserContext from '../../../hooks/useUserContext';
+import CreateProjectForm from '../components/CreateProjectForm/CreateProjectForm';
 
 export default function SprintPlanningPage() {
   const {
@@ -82,57 +82,12 @@ export default function SprintPlanningPage() {
 
   if (!project) {
     return (
-      <div className='p-3 d-flex flex-column justify-content-center align-items-center'>
-        <h2 className='fw-bold'>Create Project</h2>
-        <p className='text-muted'>
-          Please create a project to start sprint planning and gain access to a Kanban Board and
-          Project Roadmap to examine dependencies.
-        </p>
-        <Form className='mt-3'>
-          <Form.Group controlId='projectName'>
-            <Form.Label>Project Name</Form.Label>
-            <Form.Control
-              type='text'
-              placeholder='Enter Project Name'
-              value={newProject.name}
-              onChange={e => setNewProject({ ...newProject, name: e.target.value })}
-            />
-          </Form.Group>
-          <Form.Group controlId='projectDescription'>
-            <Form.Label>Project Description</Form.Label>
-            <Form.Control
-              type='text'
-              placeholder='Enter Project Description'
-              value={newProject.description}
-              onChange={e => setNewProject({ ...newProject, description: e.target.value })}
-            />
-          </Form.Group>
-          <Form.Group controlId='projectMembers'>
-            <Form.Label>Project Members</Form.Label>
-            <Form.Select
-              multiple
-              defaultValue={[]}
-              onChange={e =>
-                setNewProject({
-                  ...newProject,
-                  assignedUsers: Array.from(e.target.selectedOptions, option => option.value),
-                })
-              }>
-              {userList.map(user => (
-                <option key={user._id.toString()} value={user.username}>
-                  {user.username}
-                </option>
-              ))}
-            </Form.Select>
-          </Form.Group>
-          <Button
-            className='mt-3'
-            variant='success'
-            onClick={() => handleCreateProject(newProject)}>
-            Create Project
-          </Button>
-        </Form>
-      </div>
+      <CreateProjectForm
+        newProject={newProject}
+        setNewProject={setNewProject}
+        userList={userList}
+        handleCreateProject={handleCreateProject}
+      />
     );
   }
 
