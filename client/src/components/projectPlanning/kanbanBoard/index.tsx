bc3be3f3ede6af6b0cf6090b4ca187366906749d
Lawrence Osher <lao2205@gmail.com>
2025-03-31 19:33:34 -0400

    Added support for no project with Kanban board

--- client/src/components/projectPlanning/kanbanBoard/index.tsx
+++ client/src/components/projectPlanning/kanbanBoard/index.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Container, Row } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import KanbanBoardHeader from '../components/KanbanBoardHeader/KanbanBoardHeader';
 import ProgressColumn from '../components/ProgressColumn/ProgressColumn';
 import BacklogColumn from '../components/ProgressColumn/BacklogColumn';
@@ -11,6 +13,8 @@ import useKanbanBoardPage from '../../../hooks/useKanbanBoardPage';
 export default function KanbanBoardPage() {
   const { project } = useSelector((state: any) => state.projectReducer);
 
+  const navigate = useNavigate();
+
   const {
     progressColumns,
     showCreateTaskModal,
@@ -20,6 +24,16 @@ export default function KanbanBoardPage() {
     handleCloseCompleteSprintModal,
     handleShowCompleteSprintModal,
   } = useKanbanBoardPage();
+
+  useEffect(() => {
+    if (!project) {
+      navigate('/project/sprint-planning');
+    }
+  }, [navigate, project]);
+
+  if (!project) {
+    return null;
+  }
 
   return (
     <div className='p-3'>
@@ -43,7 +57,7 @@ export default function KanbanBoardPage() {
 
       <Container className='bg-transparent mt-3' fluid>
         <Row>
-          <BacklogColumn projectBacklog={project.backlog.tasks} />
+          <BacklogColumn projectBacklog={project.backlogTasks} />
 
           {/* Progress Columns */}
           {progressColumns.map((status: string) => (
